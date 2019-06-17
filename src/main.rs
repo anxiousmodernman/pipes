31a29657eda258a47f19da031c62ab4e208462fa
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-16 21:28:20 -0400

    steal errno from ion

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,14 @@ fn handle_sigint(sig: usize) {
     println!("\nhandlin' it! {}", sig);
     let pid = CHILD.load(Ordering::Relaxed);
     println!("awaiting child pid: {}", pid as i32);
-    let mut ptr: *const i32 = null();
-    unsafe {
-        let res: pid_t = libc::waitpid(pid as i32, ptr as *mut i32, 0);
-        println!("waitpid: {}", res);
-    }
+    let mut ptr: i32 = 0;
+        match unsafe { libc::waitpid(pid as i32, &mut ptr, 0) } {
+            -1 =>  println!{ "errno: {}", errno()},
+            pid =>  println!("waitpid: {}", pid)
+        }
     std::process::exit(42);
 }
+
+// From ion shell: https://gitlab.redox-os.org/redox-os/ion/blob/master/members/sys/src/sys/unix/mod.rs#L40
+#[cfg(target_os = "linux")]
+fn errno() -> i32 { unsafe { *libc::__errno_location() } }
