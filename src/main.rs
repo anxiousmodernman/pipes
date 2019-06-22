83152c27888f10f74ccc5abdcbdca7061a3d5d93
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-21 21:33:44 -0400

    sketch out tokio-process version of piped output (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use std::process::Command;
+use tokio_process::CommandExt; // extension trait for std::process::Command;
 use std::process::Stdio;
 use libc;
-use libc::pid_t;
 use std::thread::sleep;
 use std::time::Duration;
 use std::sync::atomic::{AtomicU32, Ordering};
-use lazy_static::lazy_static;
-use std::ptr::null;
 
 static CHILD: AtomicU32 = AtomicU32::new(0);
 
 fn main() {
-    let mut writer = Command::new("./write-forever.sh")
+
+    // foreground_std_lib();
+    foreground_tokio();
+
+    loop { sleep(Duration::from_millis(100)) }
+}
+
+fn foreground_std_lib() {
+    let writer = Command::new("./write-forever.sh")
         .stdout(Stdio::piped()).spawn().unwrap();
 
-    let mut reader = Command::new("./read_forever")
+    let reader = Command::new("./read_forever")
         .stdin(writer.stdout.expect("writer stdout error")).spawn().unwrap();
 
     // put child pid in global
@@ -23,8 +29,20 @@ fn main() {
     CHILD.store(pid, Ordering::Relaxed);
 
     unsafe { libc::signal(libc::SIGINT, handle_sigint as libc::sighandler_t) };
+}
 
-    loop { sleep(Duration::from_millis(100)) }
+fn foreground_tokio() {
+    let mut writer = Command::new("./write-forever.sh");
+    writer.stdout(Stdio::piped());
+    
+    let mut reader = Command::new("./read_forever");
+    reader.stdin(Stdio::piped());
+    let mut wr = writer.spawn_async().unwrap().stdout().take().unwrap();
+    let mut rd = reader.spawn_async().unwrap().stdin().take().unwrap();
+
+    use std::io;
+    // What's the Tokio equivalent of this?
+    // io::copy(&mut wr, &mut rd);
 }
 
 fn handle_sigint(sig: usize) {
@@ -38,6 +56,7 @@ fn handle_sigint(sig: usize) {
     }
     std::process::exit(42);
 }
+
 
 // From ion shell: https://gitlab.redox-os.org/redox-os/ion/blob/master/members/sys/src/sys/unix/mod.rs#L40
 #[cfg(target_os = "linux")]
